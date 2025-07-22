8d2490e94cd20c94d1efbedb46f4afcc504312e9
ChavesAmaro <gon94chavesamaro@gmail.com>
2025-07-22 12:11:41 +0100

    Check de idade negativa e 30 anos

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ print(f"Olá, {nome}!")
 
 try:
     idade = int(input ("Qual é a sua idade?"))
-    if idade < 30:
+    if 0 <= idade < 30:
         print(f"É o/a {nome} e tem menos de 30 anos.")
+    elif idade == 30:
+        print(f"É o/a {nome} e tem 30 anos.")
+    elif idade < 0:
+        print("Erro! A sua idade não pode ser negativa!")
     else:
         print(f"É o/a {nome} e tem mais de 30 anos.")
 except ValueError:
