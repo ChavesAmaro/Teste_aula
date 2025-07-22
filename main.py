e4c862d9e00225030fe43dfb145bfae0c26b4200
ChavesAmaro <gon94chavesamaro@gmail.com>
2025-07-22 12:16:53 +0100

    loop para erros

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ print("Olá turma do Python!")
 print("Tudo bem!")
 nome: str = input("Qual é o seu nome? ")
 print(f"Olá, {nome}!")
-
-try:
-    idade = int(input ("Qual é a sua idade?"))
-    if 0 <= idade < 30:
-        print(f"É o/a {nome} e tem menos de 30 anos.")
-    elif idade == 30:
-        print(f"É o/a {nome} e tem 30 anos.")
-    elif idade < 0:
-        print("Erro! A sua idade não pode ser negativa!")
-    else:
-        print(f"É o/a {nome} e tem mais de 30 anos.")
-except ValueError:
-    print("Erro! Introduza um valor válido da idade!")
-    
+while True:
+    try:
+        idade = int(input ("Qual é a sua idade?"))
+        if 0 <= idade < 30:
+            print(f"É o/a {nome} e tem menos de 30 anos.")
+            break
+        elif idade == 30:
+            print(f"É o/a {nome} e tem 30 anos.")
+            break
+        elif idade < 0:
+            print("Erro! A sua idade não pode ser negativa!")
+        else:
+            print(f"É o/a {nome} e tem mais de 30 anos.")
+            break
+    except ValueError:
+        print("Erro! Introduza um valor válido da idade!")
+        
