740f684940378d6743a6d4ec6c5d988b3140cca1
ChavesAmaro <gon94chavesamaro@gmail.com>
2025-07-22 12:01:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ print("Tudo bem!")
 nome: str = input("Qual é o seu nome? ")
 print(f"Olá, {nome}!")
 
-idade = int(input ("Qual é a sua idade?"))
-if idade < 30:
-    print(f"É o/a {nome} e tem menos de 30 anos.")
-else:
-    print(f"É o/a {nome} e tem mais de 30 anos.")
+try:
+    idade = int(input ("Qual é a sua idade?"))
+    if idade < 30:
+        print(f"É o/a {nome} e tem menos de 30 anos.")
+    else:
+        print(f"É o/a {nome} e tem mais de 30 anos.")
+except ValueError:
+    print("Erro! Introduza um valor válido da idade!")
     
