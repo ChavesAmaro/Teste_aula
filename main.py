e5b435eeaa2c7b6b4fa6d1517fce6743e6948ebc
ChavesAmaro <gon94chavesamaro@gmail.com>
2025-07-22 12:20:08 +0100

    Adicionados espaços "\n"

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 print("Olá turma do Python!")
 print("Tudo bem!")
-nome: str = input("Qual é o seu nome? ")
-print(f"Olá, {nome}!")
+nome: str = input("Qual é o seu nome?\n")
+print(f"\nOlá, {nome}!")
 while True:
     try:
-        idade = int(input ("Qual é a sua idade?"))
+        idade = int(input ("\nQual é a sua idade?\n"))
         if 0 <= idade < 30:
-            print(f"É o/a {nome} e tem menos de 30 anos.")
+            print(f"\nÉ o/a {nome} e tem menos de 30 anos.")
             break
         elif idade == 30:
-            print(f"É o/a {nome} e tem 30 anos.")
+            print(f"\nÉ o/a {nome} e tem 30 anos.")
             break
         elif idade < 0:
             print("Erro! A sua idade não pode ser negativa!")
         else:
-            print(f"É o/a {nome} e tem mais de 30 anos.")
+            print(f"\nÉ o/a {nome} e tem mais de 30 anos.")
             break
     except ValueError:
         print("Erro! Introduza um valor válido da idade!")
